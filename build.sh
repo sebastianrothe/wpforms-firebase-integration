3cca96a76585471400e0c504678e72022627ecbe
Sebastian Rothe <rothe@infai.org>
2018-07-17 06:03:34 +0200

    add zip command during build

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 rm -rf wpforms-firebase-integration/vendor
 composer install --no-dev --optimize-autoloader --classmap-authoritative
 cp .htaccess.deny.all wpforms-firebase-integration/vendor/.htaccess
+
+zip -r -9 build/wpforms-firebase-integration-MASTER.zip wpforms-firebase-integration
