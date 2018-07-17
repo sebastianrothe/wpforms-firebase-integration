f627f9cccaf209dfb386b47b6bc0af30517461c5
Sebastian Rothe <rothe@infai.org>
2018-07-17 06:06:14 +0200

    get rid of sytem files in zip file

--- build.sh
+++ build.sh
@@ -3,4 +3,9 @@ rm -rf wpforms-firebase-integration/vendor
 composer install --no-dev --optimize-autoloader --classmap-authoritative
 cp .htaccess.deny.all wpforms-firebase-integration/vendor/.htaccess
 
-zip -r -9 build/wpforms-firebase-integration-MASTER.zip wpforms-firebase-integration
+zip -r -9 build/wpforms-firebase-integration-MASTER.zip wpforms-firebase-integration \
+    -x */.DS_Store \
+    -x */.git \
+    -x */.svn \
+    -x */.idea \
+    -X */__MACOSX
